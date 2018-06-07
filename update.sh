d5186d8f912ec7566ce52a16358edf66c5f2a07a
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2018-06-07 18:52:42 +0200

    reload daemons in update

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ git pull
 cabal build
 cp dist/build/MensaCampusNord/MensaCampusNord /usr/local/bin/MensaCampusNord
 cp mensacampusnord.* /etc/systemd/system/
+systemctl daemon-reload
 systemctl start mensacampusnord.service
