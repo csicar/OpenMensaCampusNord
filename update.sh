f04ddea0f49a807a18e00b6d24f9fb15f9ddcf36
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-04-07 23:50:29 +0200

    removed old files

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-cabal build
-cp dist/build/MensaCampusNord/MensaCampusNord /usr/local/bin/MensaCampusNord
-cp mensacampusnord.* /etc/systemd/system/
-systemctl daemon-reload
-systemctl start mensacampusnord.service
